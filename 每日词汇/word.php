3ce1bfb2f5e81b68b3902510c0de9bbf4dd3e590
harbinailin <harbinailin@outlook.com>
2021-12-09 18:48:48 +0800

    first commit

---
+++ 每日词汇/word.php
@@ -0,0 +1,24 @@
+<?php
+include("index.html");
+$Word_filename="word.txt";
+
+function read_word(){
+    global $Word_filename;
+    $lines=file($Word_filename);
+    $random_index=rand(0,count($lines)-1);
+    $random_line=$lines[$random_index];
+    $word_all=explode("\t",$random_line);
+    list($word,$cx,$definition)=$word_all;
+?>
+<p>
+    <?=$word?>-<?=$cx?>。<?=$definition?>
+</p>
+<?php
+}
+?>
+<p>今天的每日单词是：</p>
+<?php
+read_word();
+?>
+</body>
+</html>
