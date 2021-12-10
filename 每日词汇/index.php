14683c7aa901c6bf12f035cadc7227af7f63cd41
harbinailin <harbinailin@outlook.com>
2021-12-10 17:25:38 +0800

    ll

--- 每日词汇/index.php
+++ 每日词汇/index.php
@@ -1,15 +1,16 @@
 <?php
 include("index.html");
-$Hit_Counter_Filename="count.txt";
+$Hit_Counter_Filename = "count.txt";
 
-function hit_count(){
+function hit_count()
+{
     global $Hit_Counter_Filename;
-    $hit=0;
-    if(file_exists($Hit_Counter_Filename)){
-        $hit=file_get_contents($Hit_Counter_Filename);
+    $hit = 0;
+    if (file_exists($Hit_Counter_Filename)) {
+        $hit = file_get_contents($Hit_Counter_Filename);
     }
     $hit++;
-    file_put_contents($Hit_Counter_Filename,$hit);
+    file_put_contents($Hit_Counter_Filename, $hit);
     return $hit;
 }
 
@@ -19,6 +20,7 @@ function hit_count(){
     在这里，你每次访问我们每日词汇页面，都会随机产生10个单词，方便您备考背诵。
 </p>
 <p><a href="word.php">点此处查看今天的每日单词</a></p>
-<p>该页面已经被访问<?=hit_count()?>次</p>
+<p>该页面已经被访问<?= hit_count() ?>次</p>
 </body>
+
 </html>
--- 每日词汇/word.php
+++ 每日词汇/word.php
@@ -1,24 +1,28 @@
 <?php
 include("index.html");
-$Word_filename="word.txt";
+$Word_filename = "word.txt";
 
-function read_word(){
+function read_word()
+{
     global $Word_filename;
-    $lines=file($Word_filename);
-    $random_index=rand(10,count($lines)-1);
-    $random_line=$lines[$random_index];
-    $word_all=explode("\t",$random_line);
-    list($word)=$word_all;
+    $lines = file($Word_filename);
+    $random_index = rand(0, count($lines) - 1);
+    $random_line = $lines[$random_index];
+    $word_all = explode("\t", $random_line);
+    list($word) = $word_all;
 ?>
-<p>
-    <?=$word?>
-</p>
+    <p>
+        <?= $word ?>
+    </p>
 <?php
 }
 ?>
 <p>今天的每日单词是:</p>
 <?php
-read_word();
+for ($i = 0; $i < 10; $i++) {
+    read_word();
+}
 ?>
 </body>
+
 </html>
